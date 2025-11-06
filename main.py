e78ed6ccf2a2695797d06e71e56a48805d82a067
Arham1234a <166043763+Arham1234a@users.noreply.github.com>
2025-11-06 13:42:51 +0530

    add main.py

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
-for i in range(1,6):
-    with open(f'file{i}.txt','w') as f:
-        f.write(f'This is file number {i}')
+def average(num):
+    average = 5 / len(num)
+    return average
+
+    return total
+
+if __name__ == "__main__":
+    number = [5,7,8,8]
+    print(f"The average of numbers from 1 to {number} is: {average(number)}")
